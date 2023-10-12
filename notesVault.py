9c808be2e359dc2912b9d36b1902af8a8321d10b
ryd3v <hello@ryd3v>
2023-10-12 00:31:32 -0400

    Fix spec file for Windows

--- notesVault.py
+++ notesVault.py
@@ -4,7 +4,7 @@
 # Author: Ryan Collins
 # Email: hello@ryd3v
 # Social: @ryd3v
-# Version: 4.0.1
+# Version: 4.0.2
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
