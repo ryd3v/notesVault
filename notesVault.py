b7eb196ce17e328da4b0b5b2726558989d289782
ryd3v <hello@ryd3v.com>
2023-12-16 18:21:42 -0500

    update packages

--- notesVault.py
+++ notesVault.py
@@ -4,7 +4,7 @@
 # Author: Ryan Collins
 # Email: hello@ryd3v
 # Social: @ryd3v
-# Version: 4.0.5
+# Version: 4.0.6
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
